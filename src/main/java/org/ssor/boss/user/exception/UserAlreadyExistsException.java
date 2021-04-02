d2de172cf7ccbf34e029efd02c126ae853f408b8
John Christman <80284763+LeftRuleMatters@users.noreply.github.com>
2021-04-02 15:38:35 -0500

    Features/SSOR-35-Register_User (#1)
    * Tests and beginnings
    
    * Improved tests
    
    * User creation
    
    * Repository tests
    
    * SSOR-35 Feat: Register User

---
+++ src/main/java/org/ssor/boss/user/exception/UserAlreadyExistsException.java
@@ -0,0 +1,11 @@
+package org.ssor.boss.user.exception;
+
+public class UserAlreadyExistsException extends RuntimeException
+{
+  public static final String USER_TAKEN_MESSAGE = "User with email or username already taken";
+
+  public UserAlreadyExistsException()
+  {
+    super(USER_TAKEN_MESSAGE);
+  }
+}

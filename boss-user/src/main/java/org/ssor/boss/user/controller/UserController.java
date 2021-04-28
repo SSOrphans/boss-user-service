0b9d4656ab33b8e641db8a8077fac2a0bf2e874e
christianangeles <christian.angeles@smoothstack.com>
2021-04-28 08:02:09 -0700

    SSOR-50 Feat: add COR annotation testing

--- boss-user/src/main/java/org/ssor/boss/user/controller/UserController.java
+++ boss-user/src/main/java/org/ssor/boss/user/controller/UserController.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -33,6 +34,7 @@ import static org.springframework.http.MediaType.APPLICATION_XML_VALUE;
 
 @RestController
 @RequestMapping(produces = { MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE })
+@CrossOrigin
 public class UserController {
 	public static final String USERS_ROUTE = "/api/v1/users";
 
